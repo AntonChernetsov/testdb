badbdf7806a6c00a89ab8af2cee976540258c912
Anton <ritm1c@yandex.ru>
2020-06-02 14:37:16 +0300

    1.3

--- src/main/java/com/example/mytestdb/model/Movie.java
+++ src/main/java/com/example/mytestdb/model/Movie.java
@@ -1,10 +1,33 @@
 package com.example.mytestdb.model;
 
+import java.util.Map;
+
 public class Movie {
     private int id;
     private String name;
     private int year;
-    private int imdb_rating;
+    private String imdb;
+    private float imdb_rating;
+
+    public String getImdb() {
+        return imdb;
+    }
+
+    public void setImdb(String imdb) {
+        this.imdb = imdb;
+    }
+
+    public void setImdbData(Map<String,String> map){
+        if(map.containsKey("year"))
+            try {
+                this.setYear(Integer.parseInt(map.get("year")));
+            } catch (NumberFormatException e) {}
+        if(map.containsKey("rating"))
+            try {
+                this.setImdb_rating(Float.parseFloat(map.get("rating")));
+            } catch (NumberFormatException e) {}
+
+    }
 
     public int getId() {
         return id;
@@ -30,11 +53,11 @@ public class Movie {
         this.year = year;
     }
 
-    public int getImdb_rating() {
+    public float getImdb_rating() {
         return imdb_rating;
     }
 
-    public void setImdb_rating(int imdb_rating) {
+    public void setImdb_rating(float imdb_rating) {
         this.imdb_rating = imdb_rating;
     }
 }
--- src/main/java/com/example/mytestdb/service/MovieServiceImpl.java
+++ src/main/java/com/example/mytestdb/service/MovieServiceImpl.java
@@ -1,12 +1,23 @@
 package com.example.mytestdb.service;
 
 import com.example.mytestdb.model.Movie;
-import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Service;
+import sun.net.www.http.HttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.*;
 
 @Service
 public class MovieServiceImpl implements MovieService{
@@ -17,7 +28,51 @@ public class MovieServiceImpl implements MovieService{
     @Override
     public void create(Movie movie) {
         movie.setId(++id_init);
+        try {
+            movie.setImdb(getImdbId(movie.getName()));
+            movie.setImdbData(getImdbData(movie.getImdb()));
+
+
+        } catch (IOException | UnirestException e) {
+            e.printStackTrace();
+        }
         movie_map.put(id_init,movie);
+    }
+
+    private Map<String,String> getImdbData(String imdb) throws IOException, UnirestException {
+        Map<String,String> map = new HashMap<>();
+
+        HttpResponse<String> response = Unirest.get("https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/"+imdb)
+                .header("x-rapidapi-host", "imdb-internet-movie-database-unofficial.p.rapidapi.com")
+                .header("x-rapidapi-key", "7c2daf4fa6msh1d45c7b5896722ep1c80aejsna56760b05ffa")
+                .asString();
+
+        byte[] jsonData = response.getBody().getBytes();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(jsonData);
+        map.put("year",rootNode.path("year").textValue());
+        map.put("rating",rootNode.path("rating").textValue());
+        return map;
+    }
+
+    private String getImdbId(String name) throws IOException, UnirestException {
+
+        HttpResponse<String> response = Unirest.get("https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/"+name)
+                .header("x-rapidapi-host", "imdb-internet-movie-database-unofficial.p.rapidapi.com")
+                .header("x-rapidapi-key", "7c2daf4fa6msh1d45c7b5896722ep1c80aejsna56760b05ffa")
+                .asString();
+
+        byte[] jsonData = response.getBody().getBytes();
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(jsonData);
+        JsonNode movieTitles = rootNode.path("titles");
+        Iterator<JsonNode> elements = movieTitles.elements();
+        if(elements.hasNext()) {
+            JsonNode title = elements.next();
+            JsonNode movieTitle = title.path("id");
+            return movieTitle.textValue();
+        }
+        return "na";
     }
 
     @Override
